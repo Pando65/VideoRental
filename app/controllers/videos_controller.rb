0420cae915c93be3ff64bd14b390ae09318a9d80
Pando65 <omjrrz@outlook.com>
2015-05-14 13:23:22 -0500

    add functionality of insert, delete and modify records in video table

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,17 @@
 class VideosController < CrudController::Base
+    def new 
+        @video = Video.new
+    end
+    def create 
+        @video = Video.new(video_params)
+        if @video.save 
+            redirect_to q1_path
+        else
+            render 'new'
+        end
+    end 
+    private
+    def video_params
+        params.require(:video).permit(:title, :certificate, :category, :dailyRental, :price, :director_id) 
+    end
 end
