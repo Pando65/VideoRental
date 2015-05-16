86d5cf7c96edd8614d31ccea7e7fcbcd19244b9c
Pando65 <omjrrz@outlook.com>
2015-05-16 12:39:29 -0500

    add member collection and fix nav bar

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,18 @@
 class MembersController < CrudController::Base
+    def new 
+        @object = Member.new
+    end
+    def create 
+        @object = Member.new(member_params)
+        if @object.save 
+            redirect_to members_path
+        else
+            render 'new'
+        end
+    end 
+    private
+    def member_params
+        params.require(:member).permit(:fName, :lName, :sex, :DOB, :address, :dateJoined)
+    end
+        
 end
